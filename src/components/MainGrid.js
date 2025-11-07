f0ecf3e5ec58a14e4ec6b5ab33ada63e0ebe0d17
Kael-Yann <kael.leg@gmail.com>
2025-11-07 17:13:46 +0100

    Modif pour projets scrollables sur la même page

--- src/components/MainGrid.js
+++ src/components/MainGrid.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import About from '@/components/About';
 import Contact from '@/components/Contact';
 import Intro from '@/components/Intro';
@@ -11,10 +11,27 @@ import Work from '@/components/Work';
 import { useGlobalTimeline } from '@/hooks/useAnimation';
 import LoadingBar from '@/components/LoadingBar';
 import { DISABLE_LOADING_ANIMATION } from '@/config';
+import data from '@/data.json';
 
-export default function MainGrid({ data = {} }) {
+export default function MainGrid() {
   const [loaded, setLoaded] = useState(DISABLE_LOADING_ANIMATION);
+  const [currentProject, setCurrentProject] = useState(0);
   const tl = useGlobalTimeline(loaded);
+
+  // Scroll listener pour changer le projet actif
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const sectionHeight = window.innerHeight;
+      const index = Math.min(
+        data.work.projects.length - 1,
+        Math.floor(scrollY / sectionHeight)
+      );
+      setCurrentProject(index);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
     <>
@@ -22,43 +39,47 @@ export default function MainGrid({ data = {} }) {
         <LoadingBar onFinish={() => setLoaded(true)} />
       )}
 
-      {/* Bento Grid */}
-      <div className='grid h-screen max-h-[75rem] min-h-[50rem] grid-cols-12 grid-rows-10 gap-4 p-4 max-lg:h-auto max-lg:max-h-none max-lg:grid-rows-none max-lg:py-6'>
-        <div className='col-span-full row-span-1'>
-          <Nav data={data?.nav} timeline={tl} />
-        </div>
-
-        {/* Left column */}
-        <div className='col-span-8 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4'>
-          <div className='col-span-5 row-span-5 max-lg:col-span-8 max-md:col-span-full'>
-            <Intro data={data?.intro} timeline={tl} />
+      {/* Conteneur scrollable */}
+      <div className="relative min-h-[500vh] bg-neutral-950 text-white">
+        {/* Grille fixe */}
+        <div className="fixed inset-0 grid grid-cols-12 grid-rows-10 gap-4 p-4">
+          <div className="col-span-full row-span-1">
+            <Nav data={data.nav} timeline={tl} />
           </div>
 
-          <div className='col-span-3 row-span-5 max-lg:col-span-4 max-md:col-span-full'>
-            <Portrait data={data?.portrait} timeline={tl} />
+          {/* Left column */}
+          <div className="col-span-8 row-span-9 grid grid-cols-subgrid grid-rows-subgrid gap-4">
+            <div className="col-span-5 row-span-5">
+              <Intro data={data.intro} timeline={tl} />
+            </div>
+
+            <div className="col-span-3 row-span-5">
+              <Portrait data={data.portrait} timeline={tl} />
+            </div>
+
+            <div className="col-span-4 row-span-4">
+              <About data={data.about} timeline={tl} />
+            </div>
+
+            <div className="col-span-4 row-span-4">
+              <Contact data={data.contact} timeline={tl} />
+            </div>
           </div>
 
-          <div className='col-span-4 row-span-4 max-lg:col-span-6 max-lg:min-h-[20rem] max-md:col-span-full'>
-            <About data={data?.about} timeline={tl} />
-          </div>
+          {/* Right column */}
+          <div className="col-span-4 row-span-9 grid grid-cols-subgrid grid-rows-subgrid gap-4">
+            <div className="col-span-4 row-span-8">
+              <Work
+                data={data.work}
+                timeline={tl}
+                currentProject={currentProject}
+                setCurrentProject={setCurrentProject}
+              />
+            </div>
 
-          <div className='col-span-4 row-span-4 max-lg:col-span-6 max-lg:min-h-[20rem] max-md:hidden'>
-            <Contact data={data?.contact} timeline={tl} />
-          </div>
-        </div>
-
-        {/* Right column */}
-        <div className='col-span-4 row-span-9 grid grid-cols-subgrid grid-rows-subgrid max-lg:col-span-full max-lg:grid-rows-none max-lg:gap-4'>
-          <div className='col-span-4 row-span-8 max-lg:col-span-full'>
-            <Work data={data?.work} timeline={tl} />
-          </div>
-
-          <div className='col-span-full hidden max-lg:min-h-[20rem] max-md:block'>
-            <Contact data={data?.contact} timeline={tl} />
-          </div>
-
-          <div className='col-span-4 row-span-1 max-lg:col-span-full max-lg:min-h-[5rem]'>
-            <Socials data={data?.socials} timeline={tl} />
+            <div className="col-span-4 row-span-1">
+              <Socials data={data.socials} timeline={tl} />
+            </div>
           </div>
         </div>
       </div>
